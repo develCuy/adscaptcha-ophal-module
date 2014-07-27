f97c39d4ab0c0db52110c7399a053f33856d8b4f
Fernando Paredes Garcia <fernando@develcuy.com>
2014-07-26 20:28:23 -0500

    Upgrade to Ophal-HEAD and rename variable 'asConfig' to standard 'config'.

--- init.lua
+++ init.lua
@@ -1,22 +1,25 @@
+local config = settings.adscaptcha
 local AdsCaptcha, _SERVER, add_js = require 'adscaptcha', _SERVER, add_js
-local acConfig, type = settings.adscaptcha, type
+local type = type
 
 module 'ophal.modules.adscaptcha'
 
 function form_alter(variables)
   if variables.id == 'comment_form' then
     add_js{'modules/adscaptcha/adscaptcha.js'}
-    variables.elements[1 + #variables.elements] = {'markup', value = AdsCaptcha.getCaptcha(acConfig.captchaId, acConfig.publicKey), weight = -1}
+    variables.elements[1 + #variables.elements] = {'markup', value = AdsCaptcha.getCaptcha(config.captchaId, config.publicKey), weight = -1}
   end
 end
 
 function entity_before_save(variables)
   local res
 
+  if not config.entities[entity.type] then return end
+
   if variables.adscaptcha_challenge_field and variables.adscaptcha_response_field then
     res = AdsCaptcha.validateCaptcha(
-      acConfig.captchaId,
-      acConfig.privateKey,
+      config.captchaId,
+      config.privateKey,
       variables.adscaptcha_challenge_field,
       variables.adscaptcha_response_field,
       _SERVER 'REMOTE_ADDR'
